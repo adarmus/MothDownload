5cf468ccd8c7f6b39af9ba3a7b48eec92096cf9f
adarmus <adam@blairfamily.co.uk>
2016-04-04 13:40:43 +0100

    First working version
    Bulk download - not very elegant

---
+++ getPagesSource.js
@@ -0,0 +1,61 @@
+// @author Rob W <http://stackoverflow.com/users/938089/rob-w>
+// Demo: var serialized_html = DOMtoString(document);
+
+function DOMtoString(document_root) {
+    var html = '';
+    var node = document_root.firstChild;
+    while (node) {
+        switch (node.nodeType) {
+        case Node.ELEMENT_NODE:
+            html += node.outerHTML;
+            break;
+        case Node.TEXT_NODE:
+            html += node.nodeValue;
+            break;
+        case Node.CDATA_SECTION_NODE:
+            html += '<![CDATA[' + node.nodeValue + ']]>';
+            break;
+        case Node.COMMENT_NODE:
+            html += '<!--' + node.nodeValue + '-->';
+            break;
+        case Node.DOCUMENT_TYPE_NODE:
+            // (X)HTML documents are identified by public identifiers
+            html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
+            break;
+        }
+        node = node.nextSibling;
+    }
+    return html;
+}
+
+function GetUrls(document_root) {
+  var html = '';
+  var doc = DOMtoString(document_root);
+
+  var first = doc.indexOf('<div id="stories">');
+  //html = html + 'first=' + first;
+
+  var ia = doc.indexOf('<a class="listen btn"', first);
+  //html = html + 'ia=' + ia;
+
+  while (ia > 0){
+
+	  ihttp = doc.indexOf('http:', ia + 1);
+      //html = html + 'ihttp=' + ihttp;
+	  imp3 = doc.indexOf('.mp3', ihttp) + 4;
+      //html = html + 'imp3=' + imp3;
+      url = doc.substring(ihttp, imp3);
+      //html = 'url=' + url;
+      url2 = url.replace(/\\\//g, '/');
+      html = html + '' + url2 + ';';
+
+	  ia = doc.indexOf('<a class="listen btn"', ia + 1);
+
+}
+  return html;
+}
+
+chrome.runtime.sendMessage({
+    action: "getSource",
+    source: GetUrls(document)
+});
---
+++ popup.js
@@ -0,0 +1,35 @@
+chrome.runtime.onMessage.addListener(function(request, sender) {
+  if (request.action == "getSource") {
+	var urls = request.source.split(';');
+
+	for	(index = 0; index < urls.length; index++) {
+	  var u = urls[index];
+
+      chrome.downloads.download({
+  	    url: u
+      });
+    }
+
+
+    message.innerText = request.source;
+  }
+});
+
+function onWindowLoad() {
+
+  var message = document.querySelector('#message');
+
+  chrome.tabs.executeScript(null, {
+    file: "getPagesSource.js"
+  }, function() {
+    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+    if (chrome.runtime.lastError) {
+      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+    }
+  });
+
+}
+
+window.onload = onWindowLoad;
+
+
